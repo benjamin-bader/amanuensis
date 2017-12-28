cf9efaf2135af9b3988d30efb7376d3c851e557f
Ben Bader <bebader@microsoft.com>
2017-12-27 18:24:46 -0600

    Try to get QT bins into PATH on macOS

--- .ci/install-deps.sh
+++ .ci/install-deps.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 # Install the correct dependencies, depending on the current OS.
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   # Installs QT 5.9.1, specifically
-  brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/68034aae6836950da231b01eb64b94d07e15276b/Formula/qt.rb
+  brew install qt@5.9.1 # https://raw.githubusercontent.com/Homebrew/homebrew-core/68034aae6836950da231b01eb64b94d07e15276b/Formula/qt.rb
+   echo 'export PATH="$(brew --prefix qt@5.9.1)/bin:$PATH"' >> ~/.bashrc
   export QTDIR=$(brew --prefix)/Cellar/qt/5.9.1
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
