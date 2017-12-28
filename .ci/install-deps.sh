3c21d48a356d7a5ebeb345680126e5c46703323b
Ben Bader <bebader@microsoft.com>
2017-12-27 22:41:58 -0600

    Fix location of trusty-constants.pri in install-deps.sh

--- .ci/install-deps.sh
+++ .ci/install-deps.sh
@@ -19,10 +19,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
   # Now we need to configure the build to use the new cert
   KEYHASH=$(sudo security find-certificate -c "Amanuensis Authors" -Z login.keychain | awk '/^SHA-1/ { print $3}')
-  rm trusty-constants/trusty-constants.pri
-  echo 'CERT_CN = "\"Amanuensis Authors\""' > trusty-constants/trusty-constants.pri
-  echo 'CERT_OU = bendb.com' >> trusty-constants/trusty-constants.pri
-  echo "CERTSHA1 = $KEYHASH" >> trusty-constants/trusty-constants.pri
+  rm trusty-constants.pri
+  echo 'CERT_CN = "\"Amanuensis Authors\""' > trusty-constants.pri
+  echo 'CERT_OU = bendb.com' >> trusty-constants.pri
+  echo "CERTSHA1 = $KEYHASH" >> trusty-constants.pri
 
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
